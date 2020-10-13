e3287bc5f1aac25835797086460bc06916e3da66
JoshuaTerra <joshuaterra12@hotmail.com>
2020-10-13 14:23:18 +0200

    First commit

---
+++ Traffic-Simulation/TrafficRoad/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficRoad
+{
+    public partial class TrafficLight : Form
+    {
+        public TrafficLight()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
