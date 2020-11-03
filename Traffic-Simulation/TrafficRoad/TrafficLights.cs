fa58c304021655b1d8d9ec1715a0c26ece913bbd
reinderhe <shev22ppal@live.nl>
2020-11-03 13:13:46 +0100

    Create TrafficLights.cs
    TrafficLights.cd Added

---
+++ Traffic-Simulation/TrafficRoad/TrafficLights.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficRoad
+{
+    
+
+    class TrafficLights : Traffic
+    {
+ 
+        public void InitTrafficLights(int xposition, int yposition)
+        {
+            tl = new PictureBox();
+
+            tl.Image = Properties.Resources.light_stop;
+
+            tl.Location = new Point(xposition, yposition);
+
+            tl.Size = new Size(7, 16);
+
+            tl.BackColor = Color.Transparent;
+
+            tl.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
