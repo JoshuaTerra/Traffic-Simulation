2c36079e24421c92380ea0f7d7a2592c900f7006
robinsinnema <robin.sinnema@student.nhlstenden.com>
2020-11-06 14:34:38 +0100

    traffic turning added

--- Traffic-Simulation/TrafficRoad/Traffic.cs
+++ Traffic-Simulation/TrafficRoad/Traffic.cs
@@ -12,13 +12,10 @@ namespace TrafficRoad
     class Traffic
     {
         public PictureBox trafficPB;
-
         protected Road road = null;
-
-        public string direction = "";
-
-        protected int width = 15;
-        protected int height = 30;
+        protected string direction = "";
+        private bool isFlipped = false;
+        public int prevRotation = 0;
 
         public void spawnTraffic(int leftX, int topY, string direction, Road road)
         {
@@ -43,41 +40,163 @@ namespace TrafficRoad
 
         public void movement(int speed)
         {
-            //int roadX = road.leftX;
-            //int roadY = road.topY;
-            //int carX = trafficPB.Location.X;
-            //int carY = trafficPB.Location.Y;
+            flip();
 
-            //if (carX == roadX)
-            //{
-            //    this.direction = road.direction;
-            //}
-            //if(carY > roadY)
-            //{
-            //    this.direction = road.direction;
-            //}
+            int roadX = road.leftX;
+            int roadY = road.topY;
+            int carX = trafficPB.Location.X;
+            int carY = trafficPB.Location.Y;
 
+            // if statement to set the right direction of the car when getting onto another lane (road)
+            if (this.direction == "down" && carY > roadY)
+            {
+                this.direction = road.direction;
+            }
+            else if (this.direction == "right" && carX > roadX)
+            {
+                this.direction = road.direction;
+            }
+            else if (this.direction == "up" && carY < roadY)
+            {
+                this.direction = road.direction;
+            }
+            else if (this.direction == "left" && carX < roadX)
+            {
+                this.direction = road.direction;
+            }
+
+            // if statement that moves the picturebox in the right direction and flips the image
             if (this.direction == "right")
             {
                 trafficPB.Left += 1 * speed;
                 trafficPB.Top += 0 * speed;
+                if (!isFlipped)
+                {
+                    flip();
+                    isFlipped = true;
+                }
+                trafficPB.Size = new Size(30, 15);
             }
             else if (this.direction == "down")
             {
                 trafficPB.Left += 0 * speed;
                 trafficPB.Top += 1 * speed;
+                trafficPB.Size = new Size(15, 30);
             }
             else if (this.direction == "left")
             {
                 trafficPB.Left -= 1 * speed;
                 trafficPB.Top += 0 * speed;
+                if (!isFlipped)
+                {
+                    flip();
+                    isFlipped = true;
+                }
+                trafficPB.Size = new Size(30, 15);
             }
             else if (this.direction == "up")
             {
                 trafficPB.Left += 0 * speed;
                 trafficPB.Top -= 1 * speed;
+                if (!isFlipped)
+                {
+                    flip();
+                    isFlipped = true;
+                }
+                trafficPB.Size = new Size(15, 30);
             }
+        }
 
+        public void flip()
+        {
+            if (this.direction == "right")
+            {
+                if (prevRotation == 0)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    prevRotation = 90;
+                }
+                if (prevRotation == 90)
+                {
+                    prevRotation = 90;
+                }
+                if (prevRotation == 180)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    prevRotation = 90;
+                }
+                if (prevRotation == 270)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    prevRotation = 90;
+                }
+            }
+            else if (this.direction == "down")
+            {
+                if (prevRotation == 0)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    prevRotation = 180;
+                }
+                if (prevRotation == 90)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    prevRotation = 180;
+                }
+                if (prevRotation == 180)
+                {
+                    prevRotation = 180;
+                }
+                if (prevRotation == 270)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    prevRotation = 180;
+                }
+            }
+            else if (this.direction == "left")
+            {
+                if (prevRotation == 0)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    prevRotation = 270;
+                }
+                if (prevRotation == 90)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    prevRotation = 270;
+                }
+                if (prevRotation == 180)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    prevRotation = 270;
+                }
+                if (prevRotation == 270)
+                {
+                    prevRotation = 270;
+                }
+            }
+            else if (this.direction == "up")
+            {
+                if (prevRotation == 0)
+                {
+                    prevRotation = 0;
+                }
+                if (prevRotation == 90)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    prevRotation = 0;
+                }
+                if (prevRotation == 180)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    prevRotation = 0;
+                }
+                if (prevRotation == 270)
+                {
+                    trafficPB.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    prevRotation = 0;
+                }
+            }
         }
     }
 }
