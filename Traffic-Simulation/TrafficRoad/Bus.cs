9f3256764ed6d9d9024bb764ae4bc4117465b90d
Joshua_Terra <joshuaterra12@hotmail.com>
2020-11-13 14:52:37 +0100

    Added bus, pedestrian and cyclist

---
+++ Traffic-Simulation/TrafficRoad/Bus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TrafficRoad
+{
+    class Bus : Traffic
+    {
+        public void spawnTraffic(int leftX, int topY, string direction, Road road)
+        {
+            trafficPB = new PictureBox();
+            //trafficPB.Image = Properties.Resources.ferrari;
+            trafficPB.BackColor = Color.Transparent;
+            trafficPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            trafficPB.Size = new Size(15, 30);
+
+            this.direction = direction;
+            this.road = road;
+
+            trafficPB.Left = leftX;
+            trafficPB.Top = topY;
+        }
+    }
+}
---
+++ Traffic-Simulation/TrafficRoad/Pedestrian.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TrafficRoad
+{
+    class Pedestrian : Traffic
+    {
+        public void spawnTraffic(int leftX, int topY, string direction, Road road)
+        {
+            trafficPB = new PictureBox();
+            //trafficPB.Image = Properties.Resources.ferrari;
+            trafficPB.BackColor = Color.Transparent;
+            trafficPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            trafficPB.Size = new Size(15, 30);
+
+            this.direction = direction;
+            this.road = road;
+
+            trafficPB.Left = leftX;
+            trafficPB.Top = topY;
+        }
+    }
+}
