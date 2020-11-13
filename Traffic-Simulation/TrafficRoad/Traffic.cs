2f7cf0825e3f3938488e2a1795410cb656c57413
robinsinnema <robin.sinnema@student.nhlstenden.com>
2020-11-13 13:34:32 +0100

    refactoring & traffic stops at trafficlights

--- Traffic-Simulation/TrafficRoad/Traffic.cs
+++ Traffic-Simulation/TrafficRoad/Traffic.cs
@@ -16,6 +16,7 @@ namespace TrafficRoad
         protected string direction;
         private bool isFlipped = false;
         public int prevRotation = 0;
+        public bool stop = false;
 
         public void spawnTraffic(int leftX, int topY, string direction, Road road)
         {
@@ -49,25 +50,25 @@ namespace TrafficRoad
             int carY = trafficPB.Location.Y;
 
             // if statement to set the right direction of the car when getting onto another lane (road)
-            if (carY + 2 >= roadY && direction == "down")
+            if (carY + 2 >= roadY && direction == "south")
             {
                 direction = road.direction;
             }
-            else if (carX - 2 >= roadX && direction == "right")
+            else if (carX - 2 >= roadX && direction == "east")
             {
                 direction = road.direction;
             }
-            else if (carY - 2 <= roadY && direction == "up")
+            else if (carY - 2 <= roadY && direction == "north")
             {
                 direction = road.direction;
             }
-            else if (carX + 2 <= roadX && direction == "left")
+            else if (carX + 2 <= roadX && direction == "west")
             {
                 direction = road.direction;
             }
 
             // if statement that moves the picturebox in the right direction and flips the image
-            if (direction == "right")
+            if (direction == "east" && !stop)
             {
                 trafficPB.Left += 1 * speed;
                 trafficPB.Top += 0 * speed;
@@ -78,13 +79,13 @@ namespace TrafficRoad
                 }
                 trafficPB.Size = new Size(30, 15);
             }
-            else if (direction == "down")
+            else if (direction == "south" && !stop)
             {
                 trafficPB.Left += 0 * speed;
                 trafficPB.Top += 1 * speed;
                 trafficPB.Size = new Size(15, 30);
             }
-            else if (direction == "left")
+            else if (direction == "west" && !stop)
             {
                 trafficPB.Left -= 1 * speed;
                 trafficPB.Top += 0 * speed;
@@ -95,7 +96,7 @@ namespace TrafficRoad
                 }
                 trafficPB.Size = new Size(30, 15);
             }
-            else if (direction == "up")
+            else if (direction == "north" && !stop)
             {
                 trafficPB.Left += 0 * speed;
                 trafficPB.Top -= 1 * speed;
@@ -108,9 +109,10 @@ namespace TrafficRoad
             }
         }
 
+        // this function takes care of flipping all the traffic images
         public void flip()
         {
-            if (direction == "right")
+            if (direction == "east")
             {
                 if (prevRotation == 0)
                 {
@@ -132,7 +134,7 @@ namespace TrafficRoad
                     prevRotation = 90;
                 }
             }
-            else if (direction == "down")
+            else if (direction == "south")
             {
                 if (prevRotation == 0)
                 {
@@ -154,7 +156,7 @@ namespace TrafficRoad
                     prevRotation = 180;
                 }
             }
-            else if (direction == "left")
+            else if (direction == "west")
             {
                 if (prevRotation == 0)
                 {
@@ -176,7 +178,7 @@ namespace TrafficRoad
                     prevRotation = 270;
                 }
             }
-            else if (direction == "up")
+            else if (direction == "north")
             {
                 if (prevRotation == 0)
                 {
@@ -200,6 +202,7 @@ namespace TrafficRoad
             }
         }
 
+        // deletes the traffic if they get out of the boundaries
         public void deleteTraffic()
         {
             if (trafficPB.Location.X < -20 || trafficPB.Location.X > 922)
@@ -212,36 +215,70 @@ namespace TrafficRoad
             }
         }
 
-        public bool collisionDetection(List<Traffic> traffic)
+        // collision detection function
+        public bool collisionDetection(List<Traffic> traffic, List<TrafficLight> lights)
         {
+            // creating new list with all the current traffic 
             List<Traffic> trafficList = traffic;
+            List<TrafficLight> lightsList = lights;
 
+            // if the list is empty return false
             if (trafficList.Count == 0)
             {
                 return false;
             }
 
+            // These are the collisionboxes infront of the cars for each direction
             Rectangle rectangle = new Rectangle();
-            if (direction == "up")
+            if (direction == "north")
             {
                 rectangle = new Rectangle(trafficPB.Left, trafficPB.Top - 15, trafficPB.Width, 15);
             }
 
-            if (direction == "down")
+            if (direction == "south")
             {
                 rectangle = new Rectangle(trafficPB.Left, trafficPB.Top + trafficPB.Height, trafficPB.Width, 15);
             }
 
-            if (direction == "right")
+            if (direction == "east")
             {
                 rectangle = new Rectangle(trafficPB.Left + trafficPB.Width, trafficPB.Top, 15, trafficPB.Height);
             }
 
-            if (direction == "left")
+            if (direction == "west")
             {
                 rectangle = new Rectangle(trafficPB.Left - 15, trafficPB.Top, 15, trafficPB.Height);
             }
 
+            foreach (TrafficLight tl in lightsList)
+            {
+                if (road.tl != null)
+                {
+                    if (rectangle.IntersectsWith(tl.trafficLightPB.Bounds) && road.tl.trafficLightStatus == 0)
+                    {
+                        stop = true;
+                    }
+                    else
+                    {
+                        stop = false;
+                    }
+                }
+            }
+
+            // stops the traffic infront of the trafficlights if light is red
+            //if (road.tl != null)
+            //{
+            //    if (rectangle.IntersectsWith(road.tl.trafficLightPB.Bounds) && road.tl.trafficLightStatus == 0)
+            //    {
+            //        stop = true;
+            //    }
+            //    else
+            //    {
+            //        stop = false;
+            //    }
+            //}
+
+            // if the collisionbox infront of the car intersects with another car it stops
             foreach (Traffic t in trafficList)
             {
                 if (rectangle.IntersectsWith(t.trafficPB.Bounds) && t.direction == direction)
---
+++ Traffic-Simulation/TrafficRoad/TrafficLight.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficRoad
+{
+    public class TrafficLight
+    {
+        public int flipped = 0;
+        public int trafficLightStatus;
+        public PictureBox trafficLightPB;
+
+        public void addTrafficLight(int width, int height, int leftX, int topY, int flipped, int trafficLightStatus)
+        {
+            trafficLightPB = new PictureBox();
+            trafficLightPB.Image = Properties.Resources.light_stop;
+            trafficLightPB.BackColor = Color.Transparent;
+            trafficLightPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            trafficLightPB.Width = width;
+            trafficLightPB.Height = height;
+            trafficLightPB.Left = leftX;
+            trafficLightPB.Top = topY;
+            this.flipped = flipped;
+            this.trafficLightStatus = trafficLightStatus;
+
+            if (flipped == 90)
+            {
+                trafficLightPB.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+            else if (flipped == 180)
+            {
+                trafficLightPB.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            }
+            else if (flipped == 270)
+            {
+                trafficLightPB.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            }
+        }
+
+        public void checkTrafficLightStatus()
+        {
+            if (trafficLightStatus == 0)
+            {
+                trafficLightPB.Image = Properties.Resources.light_stop;
+            }
+            else if (trafficLightStatus == 1)
+            {
+                trafficLightPB.Image = Properties.Resources.light_go;
+            }
+
+            if (flipped == 90)
+            {
+                trafficLightPB.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+            else if (flipped == 180)
+            {
+                trafficLightPB.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            }
+            else if (flipped == 270)
+            {
+                trafficLightPB.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            }   
+        }
+    }
+}
